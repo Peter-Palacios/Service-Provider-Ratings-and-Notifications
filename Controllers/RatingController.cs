c9aae3246172c7b7dee70ee17d32ee66267fb2ea
b0xitron <100628630+b0xitron@users.noreply.github.com>
2023-03-31 07:09:49 +0200

    Create Controller/RatingController

---
+++ Controllers/RatingController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service_Provider_Ratings_and_Notifications.Models;
+using Service_Provider_Ratings_and_Notifications.Services;
+
+namespace Service_Provider_Ratings_and_Notifications.Controllers
+{
+    [Route("ratings")]
+    [ApiController]
+    public class RatingController : ControllerBase
+    {
+        private readonly IRatingService rating_Service;
+        public RatingController(IRatingService ratingService)
+        {
+            rating_Service = ratingService;
+        }
+        [HttpPost]
+        public IActionResult Post([FromBody] Rating rating)
+        {
+            rating_Service.AddRating(rating);
+            NotifyNewRating(rating);
+            return Ok();
+        }
+        [HttpGet("{providerId}")]
+        public IActionResult Get(Guid providerId)
+        {
+            double averageRating = rating_Service.GetAverageRating(providerId);
+            return Ok(averageRating);
+        }
+
+        private void NotifyNewRating(Rating rating)
+        {
+
+        }
+    }
+}
