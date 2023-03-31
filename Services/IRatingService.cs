9a8a6b79bcb5809c40bdbd9642548d472cf66d3c
b0xitron <100628630+b0xitron@users.noreply.github.com>
2023-03-31 07:07:11 +0200

    create Service/Rating Services

---
+++ Services/IRatingService.cs
@@ -0,0 +1,10 @@
+﻿using Service_Provider_Ratings_and_Notifications.Models;
+
+namespace Service_Provider_Ratings_and_Notifications.Services
+{
+    public interface IRatingService
+    {
+        void AddRating(Rating rating);
+        double GetAverageRating(Guid providerId);
+    }
+}
