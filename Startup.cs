f2e5bcfe1b0677ffdcf59fc904aff9448d43ceb7
b0xitron <100628630+b0xitron@users.noreply.github.com>
2023-03-31 17:24:13 +0200

    added new Startup.cs

---
+++ Startup.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Service_Provider_Ratings_and_Notifications.Services;
+using System;
+
+public class Startup
+{
+	public Startup(IConfiguration configuration)
+	{
+		Configuration = configuration;
+	}
+	public IConfiguration Configuration { get; }
+	public void ConfigureServices(IServiceCollection services)
+	{
+		services.AddSingleton<IRatingService, RatingServiceImp>();
+		services.AddControllers();
+		services.AddEndpointsApiExplorer();
+		services.AddSwaggerGen();
+	}
+	public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+	{
+		if (env.IsDevelopment())
+		{
+			app.UseDeveloperExceptionPage();
+
+			app.UseSwagger();
+
+			app.UseSwaggerUI();
+
+		}
+
+		app.UseHttpsRedirection();
+		app.UseRouting();
+		app.UseAuthorization();
+		app.UseEndpoints(endpoints =>
+
+		{
+			endpoints.MapControllers();
+		});
+	}
+}
