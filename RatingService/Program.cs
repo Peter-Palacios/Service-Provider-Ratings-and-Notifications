2bb98cd1ab75b2432833160cdda7010f996b23dc
b0xitron <100628630+b0xitron@users.noreply.github.com>
2023-04-05 12:04:07 +0200

    program.cs useUrl()

--- RatingService/Program.cs
+++ RatingService/Program.cs
@@ -35,9 +35,19 @@ public class Program
     }
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
+    Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(webBuilder =>
+        {
+            webBuilder.UseStartup<Startup>()
+                      .UseUrls("http://*:5000"); 
+        });
+
+
+    // public static IHostBuilder CreateHostBuilder(string[] args) =>
+    //     Host.CreateDefaultBuilder(args)
+    //         .ConfigureWebHostDefaults(webBuilder =>
+    //         {
+    //             webBuilder.UseStartup<Startup>();
+    //         });
 }
+
